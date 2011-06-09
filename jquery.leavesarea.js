bffe3e6452a1a22dfc375718c20044d7accc2fd1
Michael Smith <michael.smith@cornerstonenw.com>
2011-06-08 17:43:40 -0700

    only call leave if it has been re-entered

--- jquery.leavesarea.js
+++ jquery.leavesarea.js
@@ -11,15 +11,24 @@
 (function($) {
   $.fn.leavesArea = function(callback) {
     var self = $(this);
+    var conditional = false;
     $(document).bind('mousemove', function(e) {
-      var up_mouse = self.offset().top > e.pageY,
-      left_mouse = self.offset().left > e.pageX,
-      right_mouse = self.offset().left + self.width() < e.pageX,
-      down_mouse = self.offset().top + self.height() < e.pageY;
-      if (up_mouse || left_mouse || right_mouse || down_mouse) {
-        self.trigger('left-area');
-        if (callback)
-          callback();
+      var x = e.pageX;
+      var y = e.pageY;
+      if (!conditional && x > self.offset().left && x < self.offset().left + self.width() && y > self.offset().top && y < self.offset().top + self.height()) {
+        conditional = true;
+      }
+      else {
+        var up_mouse = self.offset().top > e.pageY,
+        left_mouse = self.offset().left > e.pageX,
+        right_mouse = self.offset().left + self.width() < e.pageX,
+        down_mouse = self.offset().top + self.height() < e.pageY;
+        if ((up_mouse || left_mouse || right_mouse || down_mouse) && conditional) {
+          conditional = false;
+          self.trigger('left-area');
+          if (callback)
+            callback();
+        }
       }
     });
     return this;
